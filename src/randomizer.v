6e4f0d4109117132e5bf29b9c1aecd7270a26794
Rasmus Gundorff Sæderup <rgs@ventriject.com>
2024-03-21 00:50:51 +0100

    initial

--- src/randomizer.v
+++ src/randomizer.v
@@ -17,6 +17,7 @@ module randomizer (
 
   initial x = 18'b000000000000000001;
   initial y = 18'b111111111111111111;
+  initial r0 = 2'b00;
   
   wire z1,z2;
   wire [1:0] z12;
@@ -24,7 +25,7 @@ module randomizer (
   assign z2 = y[5] ^ y[6] ^ y[8] ^ y[9] ^ y[10] ^ y[11] ^ y[12] ^ y[13] ^ y[14] ^ y[15];
 
   assign z12 = {1'b0, z1 ^ z2}; // zn(i+131072 mod(2 18-1)) * 2
-  assign o_r = ({z12 << 1 | 2'b00}) + {1'b0, x[0] ^ y[0]};
+  assign o_r = (z12 << 1) + {1'b0, x[0] ^ y[0]};
 
   always @(posedge i_clk) begin
     //i_en_delayed <= i_en;
