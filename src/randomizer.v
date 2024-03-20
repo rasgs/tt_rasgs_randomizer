12b5f26db23ba05494f38373e3d1d2e36165fc89
Rasmus Gundorff Sæderup <rgs@ventriject.com>
2024-03-21 00:58:10 +0100

    squiggle

--- src/randomizer.v
+++ src/randomizer.v
@@ -18,13 +18,13 @@ module randomizer (
   initial x = 18'b000000000000000001;
   initial y = 18'b111111111111111111;
   
-  wire z1,z2 = 0;
-  wire [1:0] z12 = 0;
+  wire z1,z2;
+  wire z12;
   assign z1= x[4] ^ x[6] ^ x[15];
   assign z2 = y[5] ^ y[6] ^ y[8] ^ y[9] ^ y[10] ^ y[11] ^ y[12] ^ y[13] ^ y[14] ^ y[15];
 
-  assign z12 = {1'b0, z1 ^ z2}; // zn(i+131072 mod(2 18-1)) * 2
-  assign o_r = (z12 << 1) + {1'b0, x[0] ^ y[0]};
+  assign z12 = z1 ^ z2; // zn(i+131072 mod(2 18-1)) * 2
+  assign o_r = {z12, 1'b0} + {1'b0, x[0] ^ y[0]};
 
   always @(posedge i_clk) begin
     //i_en_delayed <= i_en;
