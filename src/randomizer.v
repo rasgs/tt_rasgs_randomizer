e41a0bf36b83b26188bbf2562a686ddecd899f5a
Rasmus Gundorff Sæderup <rgs@ventriject.com>
2024-03-22 15:34:38 +0100

    hardcode o_r

--- src/randomizer.v
+++ src/randomizer.v
@@ -5,7 +5,7 @@
 // see Appendix C in https://public.ccsds.org/Pubs/131x2b1e1.pdf
 
 module randomizer (
-  output reg [1:0] o_r,
+  output wire [1:0] o_r,
   input wire i_clk,
   input wire i_reset,
   input wire i_en
@@ -24,6 +24,7 @@ module randomizer (
   assign z2 = y[5] ^ y[6] ^ y[8] ^ y[9] ^ y[10] ^ y[11] ^ y[12] ^ y[13] ^ y[14] ^ y[15];
 
   assign z12 = z1 ^ z2; // zn(i+131072 mod(2 18-1)) * 2
+  assign o_r = 2'b11; //(z12 << 1) + {1'b0, x[0] ^ y[0]};
 
   always @(posedge i_clk) begin
     //i_en_delayed <= i_en;
@@ -36,7 +37,6 @@ module randomizer (
       begin
         x <= { x[7] ^ x[0], x[17:1]};
         y <= { y[10] ^ y[7] ^ y[5] ^ y[0], y[17:1]};
-        o_r <= (z12 << 1) + {1'b0, x[0] ^ y[0]};
       end
   end
 
