d6b9569eec3121f3f7bf6c18acfa6bafca889abb
Rasmus Gundorff Sæderup <rgs@ventriject.com>
2024-03-29 17:26:22 +0100

    unsync

--- src/randomizer.v
+++ src/randomizer.v
@@ -5,7 +5,7 @@
 // see Appendix C in https://public.ccsds.org/Pubs/131x2b1e1.pdf
 
 module randomizer (
-  output reg [1:0] o_r,
+  output wire [1:0] o_r,
   input wire i_clk,
   input wire i_reset,
   input wire i_en
@@ -17,14 +17,15 @@ module randomizer (
   initial x = 18'b000000000000000001;
   initial y = 18'b111111111111111111;
   
-  wire z1,z2;
+  wire z1, z2;
   wire z12;
   assign z1 = x[4] ^ x[6] ^ x[15];
   assign z2 = y[5] ^ y[6] ^ y[8] ^ y[9] ^ y[10] ^ y[11] ^ y[12] ^ y[13] ^ y[14] ^ y[15];
 
   assign z12 = z1 ^ z2; // zn(i+131072 mod(2 18-1)) * 2
+  assign o_r = {z12, x[0] ^ y[0]};
 
-  always @(posedge i_reset, posedge i_en, posedge i_clk) begin
+  always @(posedge i_reset, posedge i_clk) begin
     if (i_reset)
         begin
           x <= 18'b000000000000000001;
@@ -34,7 +35,6 @@ module randomizer (
       begin
         x <= { x[7] ^ x[0], x[17:1]};
         y <= { y[10] ^ y[7] ^ y[5] ^ y[0], y[17:1]};
-        o_r <= {z12, x[0] ^ y[0]};
       end
   end
 
--- test/test.py
+++ test/test.py
@@ -40,7 +40,7 @@ async def test_adder(dut):
     dut.ui_in.value = en_cycle[j % 6] # enable randomizer
     await RisingEdge(dut.clk)
     if en_cycle[j % 6]: # if enabled, check the output
-      assert dut.uo_out.value.integer == int(R_true[k]), f"Error at clock cycle {k}: {dut.uo_out.value.integer} != {R_true[k]}. has-been-reset={~do_rst}"
+      assert dut.uo_out.value.integer == int(R_true[k]), f"Error at clock cycle {k}: {dut.uo_out.value.integer} != {R_true[k]}. has-been-reset={not do_rst}"
       k += 1
     j += 1
     if (do_rst==True) and (j == 37): # do a reset after 37 cycles
